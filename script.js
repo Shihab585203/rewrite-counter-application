afbace9213421dcf924c987f86dda99ed5b73ab1
shihab585203 <sehabahmed50100@gmail.com>
2023-08-06 16:01:31 +0600

    first Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//Select Dom elements
+const counterEl = document.getElementById('counter');
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById('decrement');
+
+// Initial State
+const initialState = {
+    value: 0,
+};
+
+// Create reducer Function
+function counterReducer(state = initialState, action) {
+    if(action.type === "increment"){
+        return {
+            ...state,
+            value: state.value + 1,
+        };
+    } else if (action.type === "decrement"){
+        return {
+            ...state,
+            value: state.value - 1,
+        }; 
+    } else {
+        return state;
+    }
+}
+
+// Create Store
+const store = Redux.createStore(counterReducer);
+
+const render = () => {
+    const state = store.getState();
+    counterEl.innterText = state.value.toString();
+}
+
+store.subscribe(render);
+
+// button click listener
+incrementEl.addEventListener("click", ()=> {
+    store.dispatch({
+        type: 'increment',
+    });
+}); 
+
+decrementEl.addEventListener("click", ()=> {
+    store.dispatch({
+        type: 'decrement',
+    });
+}); 
